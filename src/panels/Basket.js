552de9fe99a7a2f7fbf890a2c62840785ab01cf3
d.vilnikov@gazintech.ru <vilnikov9198>
2020-09-10 11:28:33 +0300

    remove unused variable

--- src/panels/Basket.js
+++ src/panels/Basket.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import React, {useMemo, useState} from 'react';
 import { withRouter, Link } from 'react-router-dom';
 import accounting from 'accounting';
 
