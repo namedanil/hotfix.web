e44a8befec58b35823278b338486d58a78056f49
d.vilnikov@gazintech.ru <vilnikov9198>
2020-09-17 11:46:38 +0300

    Revert "remove comment & add new file"
    This reverts commit 860b6eeb

--- src/panels/Place.js
+++ src/panels/Place.js
@@ -1,6 +1,7 @@
 import React, { useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import accounting from 'accounting';
+// import PropTypes from 'prop-types';
 
 import edit from '../img/edit.svg';
 import './place.css';
